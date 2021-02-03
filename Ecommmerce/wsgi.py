02a158938cb70e453e57204dcb8abea36e2f88c6
BdVade <62808643+BdVade@users.noreply.github.com>
2021-02-03 02:42:46 +0100

    Update wsgi.py

--- Ecommmerce/wsgi.py
+++ Ecommmerce/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommmerce.Ecommmerce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommmerce.settings')
 
 application = get_wsgi_application()
